8ff7667a2b7194944ab1d891bec5967a14cc0856
Valerii <valeriykundas@gmail.com>
2022-10-10 00:31:55 +0300

    cleanup

--- vocab_db_test.go
+++ vocab_db_test.go
@@ -28,52 +28,14 @@ func TestAnkiSync(t *testing.T) {
 	ankiClient := AnkiConnectClient{serverUrl: ankiServerUrl, deckName: deckName}
 
 	// act
-	cardsToUpload := []AnkiCard{
-		{
-			DeckName:  deckName,
-			ModelName: "Basic",
-			Fields: AnkiCardFields{
-				Front: "test front",
-				Back:  "test back",
-			},
+	cardToUpload := AnkiCard{
+
+		DeckName:  deckName,
+		ModelName: "Basic",
+		Fields: AnkiCardFields{
+			Front: "test front",
+			Back:  "test back",
 		},
 	}
-	ankiClient.CreateCards(deckName, cardsToUpload)
-
-	findNotesResponse := AnkiConnectFindNotesResponseSchema{
-		Result: []int64{1483959289817, 1483959291695},
-		Error:  nil,
-	}
-
-	// todo: limit calls to 1
-	httpmock.RegisterResponder("POST", ankiServerUrl, httpmock.NewJsonResponderOrPanic(200, findNotesResponse))
-
-	notesInfoResponse := AnkiConnectNotesInfoResponseSchema{
-		Result: []AnkiConnectNotesInfoResponseResultSchema{
-			{
-				NoteID:    1502298033753,
-				ModelName: "Basic",
-				Tags:      []string{"tag", "another_tag"},
-				Fields: AnkiConnectCardFieldsSchema{
-					Front: AnkiConnectCardContentSchema{Value: "front content", Order: 0},
-					Back:  AnkiConnectCardContentSchema{Value: "back content", Order: 1},
-				},
-			},
-		},
-		Error: nil,
-	}
-	httpmock.RegisterResponder("POST", ankiServerUrl, httpmock.NewJsonResponderOrPanic(200, notesInfoResponse))
-
-	// assert
-	cardsOnAnkiServer := ankiClient.FetchCards(deckName)
-
-	if len(cardsOnAnkiServer) != len(cardsToUpload) {
-		t.Fatalf("got %d cards on server, want %d cards", len(cardsOnAnkiServer), len(cardsToUpload))
-	}
-
-	for i, card := range cardsToUpload {
-		if cardsOnAnkiServer[i] != card {
-			t.Fatal()
-		}
-	}
+	ankiClient.CreateCard(deckName, cardToUpload)
 }
